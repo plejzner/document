413250e50caac6c0cab7eff0ddc3297db7e5653b
Bob <plejzner@gmail.com>
2020-07-31 19:44:21 +0200

    move variable declaration near usage

--- public/index.php
+++ public/index.php
@@ -7,14 +7,13 @@ require __DIR__ . '/../vendor/autoload.php';
 use Application\DocumentsApplication;
 use Document\Document;
 
-$app = new DocumentsApplication();
-
 $doc = new Document();
 $doc->addHeader('Great Header', 1)
     ->addParagraph('This is first paragraph. Lorem ipsum dolor.')
     ->addHeader('Less important header', 2)
     ->addParagraph('And the second paragraph is here. Lorem ipsum dolor.');
 
+$app = new DocumentsApplication();
 $txtFilePath = $app->exportDocAsText($doc, 'my-document');
 $htmlFilePath = $app->exportDocAsHTML($doc, 'my-document');
 
